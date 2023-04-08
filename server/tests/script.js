26f6a1e0a4405ac8980bff5639b60dfa87604b94
Andrew Ihn <andrewihn11@gmail.com>
2023-04-08 14:56:49 -0700

    Added one test

--- server/tests/script.js
+++ server/tests/script.js
@@ -1,2 +1,20 @@
 import http from 'k6/http';
-import sleep from 'k6';
+import { check, group, sleep } from 'k6';
+
+export const options = {
+  // stages: [
+  //   { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+  //   // { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
+  //   { duration: '5m', target: 0 }, // ramp-down to 0 users
+  // ],
+  vus: 10,
+  duration: '30s',
+};
+
+// const BASE_URL = 'http://localhost:3000';
+
+export default () => {
+  http.get(`http://localhost:3000/qa/questions/40348`);
+
+  sleep(1);
+};

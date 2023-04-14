39737e5636362baacd88ff65bd132d8fc7940616
Andrew Ihn <andrewihn11@gmail.com>
2023-04-13 22:00:21 -0700

    fixed indexing error. increased speed tremendously

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,16 @@
 const Pool = require('pg-promise')();
 require('dotenv').config();
 
+// const db = Pool({
+//   user: 'andrewihn',
+//   host: 'localhost',
+//   database: 'test',
+//   port: 5432,
+// });
+
 const db = Pool({
   user: 'ubuntu',
-  // user: 'andrewihn',
-  host: '3.84.12.181',
-  // host: 'ec2-18-206-125-213.compute-1.amazonaws.com',
+  host: '18.212.19.154',
   database: 'test',
   password: 'pw',
   port: 5432,
